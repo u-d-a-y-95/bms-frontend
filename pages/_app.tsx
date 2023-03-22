d46db056cae7a794750e31ff9e28a7e8e885d851
UDay <uday.dev.bd@gmail.com>
2023-03-22 23:33:25 +0600

    add react-query and axios and also configure react-query

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,27 +1,42 @@
-import { AppProps } from 'next/app';
-import Head from 'next/head';
-import { MantineProvider } from '@mantine/core';
+import { AppProps } from "next/app";
+import Head from "next/head";
+import { MantineProvider } from "@mantine/core";
 import "../styles/globals.scss";
+import {
+  Hydrate,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
+import { useState } from "react";
+
 export default function App(props: AppProps) {
   const { Component, pageProps } = props;
+  const [queryClient] = useState(() => new QueryClient());
+  console.log(pageProps.dehydratedState);
 
   return (
     <>
       <Head>
         <title>Page title</title>
-        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
       </Head>
 
-      <MantineProvider
-        withGlobalStyles
-        withNormalizeCSS
-        theme={{
-          /** Put your mantine theme override here */
-          colorScheme: 'light',
-        }}
-      >
-        <Component {...pageProps} />
-      </MantineProvider>
+      <QueryClientProvider client={queryClient}>
+        <Hydrate state={pageProps.dehydratedState}>
+          <MantineProvider
+            withGlobalStyles
+            withNormalizeCSS
+            theme={{
+              colorScheme: "light",
+            }}
+          >
+            <Component {...pageProps} />
+          </MantineProvider>
+        </Hydrate>
+      </QueryClientProvider>
     </>
   );
 }
