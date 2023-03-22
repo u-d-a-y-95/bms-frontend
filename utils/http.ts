da4695d1fe19ef91c443cbc7bb75081cebcd697a
UDay <uday.dev.bd@gmail.com>
2023-03-23 01:00:44 +0600

    config axios

---
+++ utils/http.ts
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+axios.defaults.baseURL = process.env.NEXT_PUBLIC_BASE_URL
+
+class Http {
+  post(path: string, body: any, option?: any) {
+    return axios.post(path, body, option);
+  }
+}
+
+export const http = new Http();
