6576bb5cbcc4c5e34b48e7da35e7e0839df2ac59
UDay <uday.dev.bd@gmail.com>
2023-04-08 19:58:16 +0600

    add interceptor

--- utils/http.ts
+++ utils/http.ts
@@ -2,9 +2,26 @@ import axios from "axios";
 
 axios.defaults.baseURL = process.env.NEXT_PUBLIC_BASE_URL;
 
+interface IResponse {
+  status: number;
+  message: string;
+  data: any;
+}
+
+axios.interceptors.response.use(
+  (res) => {
+    return {
+      ...res.data,
+    };
+  },
+  (err) => {
+    return Promise.reject(err.response.data);
+  }
+);
+
 class Http {
   post(path: string, body: any, option?: any) {
-    return axios.post(path, body, option);
+    return axios.post<any, IResponse>(path, body, option);
   }
 }
 
