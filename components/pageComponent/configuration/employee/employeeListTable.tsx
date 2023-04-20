6f375fdc072c6eb844b5821d5e8b06ee13eaf1db
UDay <uday.dev.bd@gmail.com>
2023-04-20 14:17:24 +0600

    design employee table

---
+++ components/pageComponent/configuration/employee/employeeListTable.tsx
@@ -0,0 +1,87 @@
+import {
+  ActionIcon,
+  Badge,
+  Group,
+  Loader,
+  Pagination,
+  Table,
+  Tooltip,
+} from "@mantine/core";
+import { IconEye, IconTrash, IconWriting } from "@tabler/icons-react";
+import { FC } from "react";
+
+interface Props {
+  isLoading: boolean;
+  total: number;
+  employees: [
+    {
+      name: string;
+      email: string;
+      mobile: string;
+      status: string;
+    }
+  ];
+}
+
+export const EmployeeTableList: FC<Props> = ({
+  employees,
+  isLoading,
+  total,
+}: Props) => {
+  //   if (isLoading) return <Loader />;
+  return (
+    <>
+      <Table striped highlightOnHover withBorder withColumnBorders>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Mobile</th>
+            <th>Status</th>
+            <th style={{ width: "150px" }}>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {employees?.map((item: any, index: number) => (
+            <tr key={item.id}>
+              <td>{index + 1}</td>
+              <td>{item.name}</td>
+              <td>{item.email}</td>
+              <td>{item.mobile}</td>
+              <td>
+                <Badge color={item.status === "INACTIVE" ? "red" : ""}>
+                  {item.status}
+                </Badge>
+              </td>
+              <td>
+                <Group position="center">
+                  <Tooltip label="view" variant="light" color="gray" withArrow>
+                    <ActionIcon size={"sm"}>
+                      <IconEye />
+                    </ActionIcon>
+                  </Tooltip>
+
+                  <ActionIcon size={"sm"}>
+                    <IconWriting />
+                  </ActionIcon>
+                  <ActionIcon size={"sm"}>
+                    <IconTrash />
+                  </ActionIcon>
+                </Group>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+      <Pagination
+        size="sm"
+        mt="lg"
+        position="right"
+        total={Math.ceil(total / 10)}
+        defaultValue={1}
+        siblings={1}
+      />
+    </>
+  );
+};
